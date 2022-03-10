a3695ec31075eac5c0a8d540f8a302f7e86b38de
Manigandan P <manigandan.p@avalara.com>
2022-03-10 16:07:47 +0530

    show save status in ui

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -44,11 +44,9 @@ $(document).ready(function () {
     $(".save").click(function (event) {
 
       let annotations = $('td.textbox > input').map(function (i, el) {
-        return { "fileName": $(el).attr("name"), "textDesc": $(el).val() }
+        return { "fileName": $(el).attr("name"), "textDesc": $(el).val().trim() }
       }).get()
         .filter(d => d["textDesc"].trim() !== "")
-
-
       $.ajax({
         'url': '/api/annotations',
         'method': 'POST',
@@ -57,9 +55,13 @@ $(document).ready(function () {
         'contentType': 'application/json',
         'data': JSON.stringify({ annotations: annotations }),
         'success': function (data, status) {
+          $(event.target).parent().append(`<span>${status}</span>`)
           console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+        },
+        'error': function (xhr, status, err) {
+          $(event.target).parent().append(`<span>${err}</span>`)
+          console.log("err: " + JSON.stringify(err) + "\nStatus: " + status);
         }
-
       });
     })
 
