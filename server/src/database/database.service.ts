06b209560913c9f441e29174a60e4a8ab4ccf39c
Manigandan P <manigandan.p@avalara.com>
2022-05-18 18:26:43 +0530

    use db url from env variable

--- server/src/database/database.service.ts
+++ server/src/database/database.service.ts
@@ -1,8 +1,19 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
