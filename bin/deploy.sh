14c59f2b1cf8102882c7f7f262ba1b9d6dda0fe6
Manigandan P <manigandan.p@avalara.com>
2022-05-29 09:02:43 +0530

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,16 +2,6 @@
 
 cd ./server
 
-ls -lrht 
-
 npm install prisma
 
 npx prisma migrate deploy
-
-# # export DATABASE_URL=${DATABASE_URL}
-
-# echo "running docker compose build"
-# # docker-compose build 
-
-# echo ${DATABASE_URL}
-# # docker-compose push 
