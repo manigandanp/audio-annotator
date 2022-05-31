9ad3e588f61ceeedae4a4d46aae600a3dfefbdfd
Manigandan P <manigandan.p@avalara.com>
2022-05-31 15:24:13 +0530

    update prod url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,12 +1,10 @@
-let host: string;
+let baseUrl: string
 const annotatorPort = 3000
 if (process.env.NODE_ENV === 'production') {
-  host = 'annotator'
+  baseUrl = ''
 } else {
-  host = 'localhost'
+  baseUrl = `http://localhost:${annotatorPort}`
 }
-
-export const baseUrl = `http://${host}:${annotatorPort}`;
 
 export const titlesUrl = `${baseUrl}/api/titles`;
 export const projectsUrl = `${baseUrl}/api/projects`;
